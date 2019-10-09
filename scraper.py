6286ba4dbca338a9018d59be8d1c6ace92169c50
Alyssa Herbst <alyssa2@vt.edu>
2019-10-09 11:12:15 -0400

    make code look pretty

--- scraper.py
+++ scraper.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import urllib.parse
 import csv
 
 
-def get_page_result(row):
-    next_url = '{}{}'.format(base_url, row['href'])
-    r_next = requests.get(next_url, headers={'referer': referer})
+def get_page_result(url, referer):
+    r_next = requests.get(url, headers={'referer': referer})
     soup_next = bs(r_next.content, 'html5lib')
-    address_elems = [elem.strip() for elem in soup_next.find('address').getText().split('\n') if len(elem.strip()) > 0]
+    address_elems = [elem.strip() for elem in soup_next.find(
+        'address').getText().split('\n') if len(elem.strip()) > 0]
     h1 = soup_next.find('h1').getText().split('\n')[0]
     elem_1 = int(
         soup_next.find('dl', attrs={'class': "dl-horizontal no-margin-top"}).find('dt').find_next('dd').contents[
@@ -33,32 +32,47 @@ def get_page_result(row):
     return page_result
 
 
+def write_dict_to_file(results, filename, header):
+    with open(filename, mode='w') as file:
+        writer = csv.DictWriter(
+            file, fieldnames=header, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writeheader()
+        writer.writerows(results)
+
+
+def get_list_of_subpage_urls(url_main, base_url):
+    r = requests.get(url_main)
+    soup = bs(r.content, 'html5lib')
+    table = soup.find(
+        'table', attrs={'id': 'programsListView-listview'}).find('tbody')
+    row_urls = list(table.findAll('a'))
+    row_urls = list(filter(
+        lambda x: '/ads/Public/Programs/Detail?programId=' in x['href'], row_urls))
+    urls = ['{}{}'.format(base_url, row['href']) for row in row_urls]
+    return urls
+
+
+def write_error_urls_to_file(urls, filename):
+    with open(filename, 'w') as file:
+        file.writelines(urls)
+
+
 if __name__ == '__main__':
-    base_url='https://apps.acgme.org'
+    base_url = 'https://apps.acgme.org'
     url_main = 'https://apps.acgme.org/ads/Public/Programs/Search?stateId=33&specialtyId=&specialtyCategoryTypeId=&numCode=&city='
     output_file = 'results.csv'
     error_file = 'errors.txt'
-    r = requests.get(url_main)
-    soup = bs(r.content, 'html5lib')
-    table = soup.find('table', attrs={'id': 'programsListView-listview'}).find('tbody')
-    referer = 'https://apps.acgme.org/ads/Public/Programs/Search?stateId=33&specialtyId&specialtyCategoryTypeId=&numCode=&city='
     results = list()
     error_urls = []
-    for row in table.findAll('a'):
-        if '/ads/Public/Programs/Detail?programId=' not in row['href']:
-            continue
+    subpage_urls = get_list_of_subpage_urls(url_main, base_url)
+    for url in subpage_urls:
         try:
-            page_result = get_page_result(row)
+            page_result = get_page_result(url, url_main)
             results.append(page_result)
         except:
-            next_url = '{}{}'.format(base_url, row['href'])
-            error_urls.append(next_url)
-    with open(output_file, mode='w') as file:
-        rows = ['page_id', 'page_title', 'approved_resident_positions', 'filled_resident_positions', 'address']
-        writer = csv.DictWriter(file, fieldnames=rows, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writeheader()
-        writer.writerows(results)
-    with open(error_file, 'w') as file:
-        file.writelines(error_urls)
+            error_urls.append(url)
+    csv_headers = ['page_id', 'page_title', 'approved_resident_positions',
+                   'filled_resident_positions', 'address']
+    write_dict_to_file(results, output_file, csv_headers)
+    write_error_urls_to_file(error_urls, error_file)
     print(results)
-
